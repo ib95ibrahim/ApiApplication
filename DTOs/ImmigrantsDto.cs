0f759a135f7d210d835af08e460e1d83fec3d373
YASSMINE-bac <yassminebac97@gmail.com>
2022-06-18 21:35:54 +0100

    CRUDImmigrant

---
+++ DTOs/ImmigrantsDto.cs
@@ -0,0 +1,12 @@
+﻿using API.entities;
+
+namespace API.DTOs
+{
+    public class ImmigrantsDto : Person
+    {
+        public string Nationality { get; set; }
+        public string BirthDate { get; set; }
+        public string Vulnerability { get; set; }
+        public string LocalId { get; set; }
+    }
+}
