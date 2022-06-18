9a6c2879323468bf59d1433d8d1f29f9a8e2da53
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-18 16:34:12 +0100

    Employer CRUD Tache

---
+++ DTOs/RegisterEmployerDto.cs
@@ -0,0 +1,11 @@
+using API.entities;
+
+namespace API.DTOs;
+
+public class RegisterEmployerDto: Person
+{
+    public string EmployerType { get; set; }
+    public string TypeEquipe { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
