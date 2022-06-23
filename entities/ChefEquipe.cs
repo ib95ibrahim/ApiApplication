0469f1237bf7cf1780b9af08fbfd9874d14da737
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-23 17:26:33 +0100

    id chef equipe

--- entities/ChefEquipe.cs
+++ entities/ChefEquipe.cs
@@ -2,7 +2,6 @@
 {
     public class ChefEquipe : Employer
     {
-        public new int Id { get; set; }
         public List<Activity> Activities { get; set; }
         public List<Assistant> Assistants { get; set; }
     }
