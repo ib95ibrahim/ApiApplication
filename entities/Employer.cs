a894475f6a1e6382249cca25c2a429ca0d00e7b3
MoadC <rappelzkloyster@gmail.com>
2022-06-23 17:22:45 +0100

    no more Ids

--- entities/Employer.cs
+++ entities/Employer.cs
@@ -2,7 +2,7 @@
 {
     public class Employer : Person
     {
-        public new int Id { get; set; }
+        
         public string EmployerType { get; set; }
         public string TypeEquipe { get; set; }
         public string UserName { get; set; }
